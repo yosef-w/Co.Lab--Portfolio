7a63b03bc8fdbf8a447267113695c5576025fcf6
yosef-w <ywolday24@gmail.com>
2023-09-07 11:59:52 -0500

    update project names and descriptions

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,22 +4,22 @@ import './projects.css';
 const projectsData = [
   {
     id: 1,
-    title: 'Project 1',
-    description: 'Description for Project 1',
+    title: 'Health and Wellness',
+    description: 'Health and Wellness is a system that enables users to investigate symptoms, explore nutritional remedies for their ailments, and locate nearby healthcare providers and specialists.',
     imageUrl: '/assets/healthandwellness.png',
     externalLink: 'https://github.com/yosef-w/Health-and-Wellness',
   },
   {
     id: 2,
-    title: 'Project 2',
-    description: 'Description for Project 2',
+    title: 'Weather',
+    description: 'Weather is a user-friendly application that provides real-time weather updates for various cities, keeping users informed about current weather conditions effortlessly.',
     imageUrl: '/assets/weather.png',
     externalLink: 'https://github.com/yosef-w/Weather-App',
   },
   {
     id: 3,
-    title: 'Project 3',
-    description: 'Description for Project 3',
+    title: 'Star Map',
+    description: 'Star Map is an interactive application that enables users to explore and discover various constellations while offering educational insights to deepen their celestial knowledge.',
     imageUrl: '/assets/starmap.png',
     externalLink: 'https://github.com/yosef-w/Star-Map',
   },
